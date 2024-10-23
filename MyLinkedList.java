233343b74073817c992d8907670300848bb394db
logantormakim <ltormakim@gmail.com>
2024-10-23 14:19:09 +0000

    10/23 in progress

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,54 @@
+// Logan Torma-Kim
+// 10/23/2024
+// pd 3
 public class MyLinkedList
 {
+    private ListNode head;
+    private ListNode size;
+    public MyLinkedList() {
+        head = null; // when head = null list is empty
+        size = 0;
+    } // end constructor
+    
+    //start of methods
+    public int size() {
+        return size;
+    } // end size
+    
+    public boolean isEmpty() {
+        if (size == 0 || head == null) {
+            return true; 
+        }
+        else {
+            return false;
+        }
+    } // end isEmpty
 
+    public boolean add(Object newItem) {
+        ListNode temp = head
+        int size1 = temp.size();
+        while (temp.getNext() != null) {
+            
+        }
+    }
+
+
+
+
+
+
+    //to string
+    public String toString (){
+        ListNode temp = head;
+        out = "";
+        int val =1;
+        while (temp.getNext() != null) {
+            out+= val +":" + temp.getNext();
+            System.out.println(" ");
+            val++;
+            temp.getNext();
+        }
+        return out;
+    } // end toString
 
 }
